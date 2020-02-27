17e85a02b000a55a7a01e449bf628cfd29bb76cf
unknown <2120367@talnet.nl>
2020-02-27 14:49:18 +0100

    Uitwerking van de exercise

--- scale.php
+++ scale.php
@@ -3,11 +3,11 @@
 $num1 = $argv[1];
 $num2 = $argv[2];
 
-if($num1 === $num2){
-echo "Balanced :)";
+if($num1 === $num2) {
+    echo "Balanced :)";
 }
-else{
-echo "Niet in balans :(";
+else {
+    echo "Niet in balans :(";
 }
 
 ?>
